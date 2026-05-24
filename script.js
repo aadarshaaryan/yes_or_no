280c3e66faac4b9f8b8366b007094ddf068bcb25
Aadarsh Srivastava <aadarshaaryan84@gmail.com>
2026-05-24 09:07:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let p = document.getElementById("p")
+
+function calc() {
+    let time = Date.now()
+    let text = document.getElementById("question").value
+    let sum = 0
+
+    for(let i of text) {
+        sum += i.charCodeAt(0)
+    }
+
+    sum += time;
+
+    if(sum%4 == 0){
+        p.innerHTML = "YES"
+        
+    }else{
+        p.innerHTML = "NO"
+    }
+}
